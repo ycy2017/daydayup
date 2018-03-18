e9add8d9e7aa069de4e10d4b91007c3c47fb9788
ycy <caiyong.yin@mathartsys.com>
2018-03-18 22:57:10 +0800

    反射_2

--- trydemo/src/main/java/trydemo/invoke/InvokeDemo.java
+++ trydemo/src/main/java/trydemo/invoke/InvokeDemo.java
@@ -3,15 +3,21 @@ package trydemo.invoke;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class invokeDemo {
+public class InvokeDemo {
 
 	
 	public static void main(String[] args) {
 	
 		A a =new A();
 		try {
+			
 			Method method = a.getClass().getMethod("doWork", null);
 			method.invoke(a,null );
+			
+			//2
+			Class invokeDemoClass = Class.forName("trydemo.invoke.A");
+			A id= (A) invokeDemoClass.newInstance();
+			id.doWork();
 		} catch (NoSuchMethodException | SecurityException e) {
 			e.printStackTrace();
 		} catch (IllegalAccessException e) {
@@ -19,6 +25,10 @@ public class invokeDemo {
 		} catch (IllegalArgumentException e) {
 			e.printStackTrace();
 		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (InstantiationException e) {
 			e.printStackTrace();
 		}
 		
